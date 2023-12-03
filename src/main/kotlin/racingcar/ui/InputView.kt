27556cd22db11cdb43c558f510795574d195a7ba
김용수 <kys0522@midasit.com>
2023-12-03 16:36:22 +0900

    * 입력한 이름에 대한 예외처리

--- src/main/kotlin/racingcar/ui/InputView.kt
+++ src/main/kotlin/racingcar/ui/InputView.kt
@@ -6,6 +6,10 @@ class InputView {
     fun input() {
         print("names of car : ")
         carNames = readln().split(",")
+        carNames.forEach {
+            if (it.length > 5)
+                throw IllegalArgumentException("name length is more than 5")
+        }
         print("number of try : ")
         tryNum = readln().toInt()
     }
