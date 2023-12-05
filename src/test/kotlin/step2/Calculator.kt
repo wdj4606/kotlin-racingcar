7091b496375aee456e856f22233f18513f986311
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2023-12-05 21:02:31 +0900

    refactor: Simplify null-checks and enhance expression handling in Calculator
    Remove unnecessary null-checks and simplify expression handling in the Calculator class. Utilize Kotlin's require function for cleaner expression validation, eliminating the need for explicit null or blank checks. The parseOperator and parseOperand functions are modified to receive non-nullable arguments, assuming the input has been validated at a higher level. This refactoring aims to improve code readability and maintainability.

--- src/test/kotlin/step2/Calculator.kt
+++ src/test/kotlin/step2/Calculator.kt
@@ -1,34 +1,34 @@
 package step2
 
-import step2.InputValidator.requireNotNullOrBlank
-
 object Calculator {
     fun calc(expression: String?): Double {
-        val tokens = requireNotNullOrBlank(expression)
-            .split(" ")
+        require(!expression.isNullOrBlank()) { "Expression must not be null" }
+
+        val tokens = expression.split(" ")
 
         val firstOperand = parseOperand(tokens.first())
 
         return tokens
             .drop(1)
             .chunked(2) {
-                val operator = parseOperator(it.getOrNull(0))
-                val operand = parseOperand(it.getOrNull(1))
+                val operator = parseOperator(it[0])
+                val operand = parseOperand(it[1])
                 operator to operand
             }
             .fold(firstOperand) { acc, (operator, operand) ->
                 operator.apply(acc, operand)
             }
     }
-    private fun parseOperator(token: String?): Operator {
-        return requireNotNull(token).let {
+
+    private fun parseOperator(token: String): Operator {
+        return token.let {
             Operator.symbolOf(it)
         }
     }
 
-    private fun parseOperand(token: String?): Double {
-        return requireNotNull(token).let {
-            it.toDoubleOrNull() ?: throw IllegalArgumentException("Invalid operand: $token")
+    private fun parseOperand(token: String): Double {
+        return token.let {
+            it.toDouble()
         }
     }
 }
--- src/test/kotlin/step2/InputValidator.kt
+++
@@ -1,8 +0,0 @@
-package step2
-
-object InputValidator {
-    fun requireNotNullOrBlank(value: String?, message: String = "Input must not be null or blank."): String {
-        return value?.takeIf { it.isNotBlank() }
-            ?: throw IllegalArgumentException(message)
-    }
-}
