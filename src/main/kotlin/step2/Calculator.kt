deca860fe356688ccae5dd13a097c24f228ba8cc
cdy0826 <cdy0826@midasit.com>
2023-12-06 15:41:22 +0900

    step2 - pr 피드백 반영

--- src/main/kotlin/step2/Calculator.kt
+++ src/main/kotlin/step2/Calculator.kt
@@ -2,20 +2,27 @@ package step2
 
 object Calculator {
 
+    private val arrayDeque: ArrayDeque<String> = ArrayDeque()
+
+    private fun init(calculateValues: String) {
+        require(!calculateValues.isNullOrBlank()) { "계산식이 비어있습니다." }
+        arrayDeque.addAll(calculateValues.split(" "))
+    }
+
+    private fun convertNumber(number: String): Int {
+        return number.toIntOrNull() ?: throw IllegalArgumentException("숫자가 아닙니다.")
+    }
+
     fun calculate(calculateValues: String): Int {
-        if (calculateValues.isNullOrBlank()) {
-            throw IllegalArgumentException("계산식이 비어있습니다.")
-        }
+        init(calculateValues)
 
-        val arrayDeque: ArrayDeque<String> = ArrayDeque(calculateValues.split(" "))
-
-        var result: Int = arrayDeque.removeFirst().toInt()
-        while (arrayDeque.size > 0) {
+        while (arrayDeque.size >= 3) {
+            var first: Int = convertNumber(arrayDeque.removeFirst())
             val operator: String = arrayDeque.removeFirst()
-            val second: Int = arrayDeque.removeFirst().toInt()
-
-            result = Operator.from(operator).apply(result, second)
+            val second: Int = convertNumber(arrayDeque.removeFirst())
+            Operator.from(operator).op.apply { arrayDeque.addFirst(this(first, second).toString()) }
         }
-        return result
+        require(arrayDeque.size == 1) { "계산식이 잘못되었습니다." }
+        return convertNumber(arrayDeque.removeFirst())
     }
 }
--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -1,40 +1,55 @@
 package step2
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatThrownBy
+import io.kotest.inspectors.forAll
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.haveMessage
 
-internal class CalculatorTest : DescribeSpec({
+class CalculatorTest : DescribeSpec({
     val calculator = Calculator
 
     describe("calculate") {
         context("계산식이 비어있으면 IllegalArgumentException") {
             it("계산식이 비어있으면 IllegalArgumentException") {
                 // then
-                nullOrEmptyCalculations.forEach {
-                    assertThatThrownBy { calculator.calculate(it) }
-                        .isInstanceOf(IllegalArgumentException::class.java)
-                        .hasMessage("계산식이 비어있습니다.")
+                nullOrEmptyCalculations.forAll {
+                    val shouldThrow = shouldThrow<IllegalArgumentException> {
+                        calculator.calculate(it)
+                    }
+                    shouldThrow.message shouldBe "계산식이 비어있습니다."
                 }
             }
         }
         context("계산식이 잘못되었으면 IllegalArgumentException") {
             it("계산식이 잘못되었으면 IllegalArgumentException") {
                 // then
-                invalidCalculations.forEach {
-                    assertThatThrownBy { calculator.calculate(it) }
-                        .isInstanceOf(IllegalArgumentException::class.java)
-                        .hasMessage("지원하지 않는 연산자입니다.")
+                invalidCalculations.forAll {
+                    shouldThrow<IllegalArgumentException> {
+                        calculator.calculate(it)
+                        haveMessage("지원하지 않는 연산자입니다.")
+                    }
                 }
             }
         }
         context("정상 계산") {
             it("정상 계산") {
-                calculations.forEach {
+                calculations.forAll {
                     // when
                     val result = calculator.calculate(it.first)
                     // then
-                    assertThat(result).isEqualTo(it.second)
+                    result shouldBe it.second
+                }
+            }
+        }
+        context("숫자가 아닌 경우 IllegalArgumentException") {
+            it("숫자가 아닌 경우 IllegalArgumentException") {
+                // then
+                notNumber.forAll {
+                    shouldThrow<IllegalArgumentException> {
+                        calculator.calculate(it)
+                        haveMessage("숫자가 아닙니다.")
+                    }
                 }
             }
         }
@@ -42,10 +57,10 @@ internal class CalculatorTest : DescribeSpec({
 }
 
 ) {
-
     companion object {
-        var calculations = listOf("1 + 2" to 3, "1 + 2 + 3 + 4" to 10, "2 + 3 * 4 / 2" to 10)
-        var nullOrEmptyCalculations = listOf("", " ", "  ")
-        var invalidCalculations = listOf("1 % 2", "1 & 2")
+        val calculations = listOf("1 + 2" to 3, "1 + 2 + 3 + 4" to 10, "2 + 3 * 4 / 2" to 10)
+        val nullOrEmptyCalculations = listOf("", " ", "  ")
+        val invalidCalculations = listOf("1 % 2", "1 & 2")
+        val notNumber = listOf("1 + a", "1 + b", "1 + c")
     }
 }
