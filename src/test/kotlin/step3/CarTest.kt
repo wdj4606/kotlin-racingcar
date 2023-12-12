99973dd68d711d226c86195041f82ebb9d7a36bc
yhj0122 <yhj0122@midasit.com>
2023-12-12 19:58:29 +0900

    step3 수정

--- src/test/kotlin/step3/CarTest.kt
+++ src/test/kotlin/step3/CarTest.kt
@@ -2,12 +2,17 @@ package step3
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockkObject
 
 internal class CarTest : DescribeSpec({
 
     describe("Car") {
         context("moveForward()를 호출하면") {
             it("distance가 1 증가한다.") {
+                mockkObject(RandomUtil)
+                every { RandomUtil.getRandomNumber() } returns 5
+
                 val car = Car()
                 car.moveForward()
                 car.distance shouldBe 1
--- src/test/kotlin/step3/CarsTest.kt
+++ src/test/kotlin/step3/CarsTest.kt
@@ -1,24 +1,22 @@
 package step3
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
-import io.mockk.every
 import io.mockk.mockkObject
+import io.mockk.verify
 
 class CarsTest : DescribeSpec({
 
     describe("Cars") {
 
-        context("") {
+        context("goForward()를 호출하면") {
 
-            it("내부의 자동차가 전진한다.") {
+            it("자동차 메서드가 호출된다.") {
                 mockkObject(RandomUtil)
-
-                every { RandomUtil.getRandomNumber() } returns 5
 
                 val cars = Cars(3)
                 cars.goForward()
-                cars.cars.forEach { it.distance shouldBe 1 }
+
+                verify(exactly = 3) { Car().moveForward() }
             }
         }
     }
