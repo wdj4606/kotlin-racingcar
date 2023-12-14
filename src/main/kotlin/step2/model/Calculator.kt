e3252ea44e21036ee9306b9abefefc3358b3ab6f
yms1106 <yms1106@midasit.com>
2023-12-15 08:55:15 +0900

    [step2] 문자열 계산기 - 코드리뷰 피드백 반영

--- src/main/kotlin/step2/model/Calculator.kt
+++ src/main/kotlin/step2/model/Calculator.kt
@@ -1,23 +1,22 @@
 package step2.model
 
 object Calculator {
-    private fun checkValidFormula(formula: String): Boolean {
-        if (formula.isNullOrBlank()) throw IllegalArgumentException("formula is null or blank")
+    private fun checkValidFormula(formula: String?): List<String> {
+        require(!formula.isNullOrBlank()) { "formula is null or blank" }
 
         val splitFormulas = formula.split(" ")
         if (splitFormulas.size % 2 === 0) throw IllegalArgumentException("formula is incorrect")
 
-        return true
+        return splitFormulas
     }
 
     private fun calculateSegment(operand1: Int, operator: String, operand2: Int): Int {
         return OperatorType.of(operator).calculate(operand1, operand2)
     }
 
-    fun calculate(formula: String): Int {
-        checkValidFormula(formula)
+    fun calculate(formula: String?): Int {
+        val splitFormulas = checkValidFormula(formula)
 
-        val splitFormulas = formula.split(" ")
         var result = splitFormulas[0].toInt()
         var index = 1
 
