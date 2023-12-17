928bdcef9e127b248b876334a5f53d484581cd2f
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2023-12-17 16:19:02 +0900

    refactor: Combine Movable functionality into Car class

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -3,9 +3,19 @@ package racingcar.model
 import racingcar.dto.CarDto
 
 class Car(
-    movableStrategy: MovableStrategy = MovableStrategy { true },
-) : Movable(movableStrategy) {
+    val name: String,
+    private val movableStrategy: MovableStrategy = MovableStrategy { true },
+) {
+    var position: Int = 0
+        private set
+
+    fun move() {
+        if (movableStrategy.isMovable()) {
+            position++
+        }
+    }
+
     fun toDto(): CarDto {
-        return CarDto(position)
+        return CarDto(name, position)
     }
 }
