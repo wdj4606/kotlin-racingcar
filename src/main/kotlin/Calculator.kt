5c285f633eda857d89dc1f4ad1f73217c4b91689
hsw0312@midasit.com <hsw0312@midasit.com>
2023-12-18 12:08:14 +0900

    1-2 문자열 계산기 구현

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,23 +1,60 @@
-fun Calculator() {
-    //문자열 입력받음
-    //사칙연산 순서 X
-    fun preProcess(argument: String) : Pair<mutableListOf<int>, mutableListOf<Char>> {
+//문자열 입력받음
+//사칙연산 순서 X
+
+
+public class Calculator {
+
+    //더하기
+    //뺄셈
+    //곱셈
+    //나눗셈
+
+    public fun drive(argument: String): Double {
+        val (numberList, operatorList) = preProcess(argument)
+        return operate(numberList, operatorList)
+    }
+
+    private fun preProcess(argument: String): Pair<MutableList<Int>, MutableList<Char>> {
+        val argument : String = argument.replace(" ","");
+        if (argument.isEmpty()) {
+            throw IllegalArgumentException("there is no input")
+        }
+
         val numberList = mutableListOf<Int>()
         val operatorList = mutableListOf<Char>()
         var number = 0
-        for (token in argument){
+        for (token in argument) {
             when {
-                token.matches(Regex("[0-9]")) -> number = number * 10 + (token - '0')
-                token.matches(Regex("[+\\-*/]")) -> {
+                Regex("[0-9]").matches(token.toString()) -> number = number * 10 + (token - '0')
+                Regex("[+\\-*/]").matches(token.toString()) -> {
                     numberList.add(number)
                     operatorList.add(token)
                     number = 0
                 }
+                else -> throw IllegalArgumentException("only number and operator are allowed")
             }
         }
-        numberList.add(number)
+        numberList += number
         return Pair(numberList, operatorList)
     }
 
+    private fun operate(numberList: MutableList<Int>, operatorList: MutableList<Char>): Double {
+        var result : Double = numberList[0].toDouble()
+        for (i in 1 until numberList.size) {
+            result = when (operatorList[i - 1]) {
+                '+' -> result + numberList[i]
+                '-' -> result - numberList[i]
+                '*' -> result * numberList[i]
+                '/' -> result / numberList[i]
+                else -> throw IllegalArgumentException("only +, -, *, / are allowed")
+            }
+        }
+        return result
+    }
+}
 
+fun main() {
+    val calculator = Calculator()
+    val argument = readln()
+    println(calculator.drive(argument))
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,32 +1,41 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+//import io.mockk.spyk
+//import io.mockk.every
 
 class CalculatorTest {
+
+    private val epsilon = 1e-10
     @Test
     fun add() {
         val calculator = Calculator()
-        assertEquals(3, calculator.add(1, 2))
+        val argument = "1+2+3";
+        assert(kotlin.math.abs(6 - calculator.drive(argument)) < epsilon)
     }
 
     @Test
     fun multiply() {
         val calculator = Calculator()
-        assertEquals(6, calculator.multiply(2, 3))
+        val argument = "2*3"
+        assert(kotlin.math.abs(6 - calculator.drive(argument)) < epsilon)
     }
 
     @Test
     fun minus() {
         val calculator = Calculator()
-        assertEquals(1, calculator.minus(3, 2))
+        val argument = "3-2"
+        assert(kotlin.math.abs(1 - calculator.drive(argument)) < epsilon)
     }
     @Test
     fun divide() {
         val calculator = Calculator()
-        assertEquals(2, calculator.divide(4, 2))
+        val argument = "6/2"
+        assert(kotlin.math.abs(3 - calculator.drive(argument)) < epsilon)
     }
-    @Test
-    fun preProcess() {
-        val calculator = Calculator()
-        assertEquals(Pair(mutableListOf(1, 2, 3), mutableListOf('+', '-')), calculator.preProcess("1+2-3"))
-    }
+//    @Test
+//    fun preProcess() {
+//        val mockCalculator = spyk<Calculator>(recordPrivateCalls = true)
+//        every { mockCalculator["preProcess"]() } returns Pair(mutableListOf(1, 2, 3), mutableListOf('+', '-'))
+//        assertEquals(Pair(mutableListOf(1, 2, 3), mutableListOf('+', '-')), mockCalculator.drive("1+2-3"))
+//    }
 }
