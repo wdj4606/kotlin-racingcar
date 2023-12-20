74581de14b5bcff5cea01e9368c7e0024fa44603
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2023-12-20 23:04:06 +0900

    refactor: Trim spaces using trim(). Handle logical errors with exceptions

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -9,13 +9,18 @@ object InputView {
     fun getCarNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
         return readlnOrNull()
-            ?.replace(" ", "")
+            ?.trim()
             ?.split(",")
-            ?: listOf<String>()
+            ?: throw IllegalArgumentException("유효하지 않은 자동차 이름 입력입니다. 이름은 쉼표(,)로 구분되어야 합니다.")
     }
 
     fun getTryCount(): Int {
         println("시도할 횟수는 몇 회인가요?")
-        return readlnOrNull()?.toIntOrNull() ?: 0
+
+        return readlnOrNull()
+            ?.run {
+                toIntOrNull()?.takeIf { it > 0 }
+            }
+            ?: throw IllegalArgumentException("유효하지 않은 시도 횟수 입력입니다. 자연수로 입력되어야 합니다.")
     }
 }
