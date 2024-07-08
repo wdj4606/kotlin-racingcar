ed89f9bf5c0ec4426ab8f2d12f7fc2b4e81008bf
rtf400 <rtf400@naver.com>
2024-07-08 21:24:45 +0900

    <코드리뷰> (Step2) : Calculator
    부동소숫점 오류를 막기 위해 BigDecimal로 변경

--- src/test/kotlin/Calculator.kt
+++ src/test/kotlin/Calculator.kt
@@ -1,22 +1,25 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 object Calculator {
     fun execute(formula: String?): Double {
         val calcItems = FormulaParser.execute(formula)
 
-        var result = calcItems[0].toDouble()
+        var result = BigDecimal(calcItems[0])
 
         for (i in 1 until calcItems.size step 2) {
             val operator = calcItems[i]
-            val nextNumber = calcItems[i + 1].toDouble()
+            val nextNumber = BigDecimal(calcItems[i + 1])
 
             result = when (operator) {
                 FormulaParser.PLUS -> result + nextNumber
                 FormulaParser.MINUS -> result - nextNumber
                 FormulaParser.MULTIPLY -> result * nextNumber
-                FormulaParser.DIVIDE -> result / nextNumber
+                FormulaParser.DIVIDE -> result.divide(nextNumber, 10, RoundingMode.HALF_UP)
                 else -> throw IllegalArgumentException("Unknown operator: $operator")
             }
         }
 
-        return result
+        return result.toDouble()
     }
 }
