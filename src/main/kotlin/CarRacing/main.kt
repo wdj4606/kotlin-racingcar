226c3fce9897e35184e29f01d6747623d7093a5e
rtf400 <rtf400@naver.com>
2024-07-10 20:30:25 +0900

    <추가> (Step3) : 자동차 경주
    경주 결과 출력 클래스 추가

--- src/main/kotlin/CarRacing/main.kt
+++ src/main/kotlin/CarRacing/main.kt
@@ -1,9 +1,14 @@
 import CarRacing.InputView
 import CarRacing.Race
+import CarRacing.ResultView
 
 fun main() {
     val inputParameters = InputView.run()
     val race = Race(inputParameters)
 
-    repeat(inputParameters.numberOfRace) { race.tryRace() }
+    ResultView.showStart()
+    repeat(inputParameters.numberOfRace) {
+        race.tryRace()
+        ResultView.showRaceSnapShot(race)
+    }
 }
