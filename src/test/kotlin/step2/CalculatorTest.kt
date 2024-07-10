83c288e2cea182cb0c4a5ece4f7819034525826c
pcs0807 <pcs0807@midasit.com>
2024-07-10 23:00:59 +0900

    <추가> 재업로드

--- src/test/kotlin/step2/CalculatorTest.kt
+++ src/test/kotlin/step2/CalculatorTest.kt
@@ -1,95 +1,55 @@
 package step2
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import step2.Calculator.calculateExpression
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import step2.Calculator.calculate
 
 class CalculatorTest {
-    @Test
-    fun `기본 사칙연산`() {
 
-        calculateExpression("1 + 1").let {
-            assertThat(it).isEqualTo(2.0)
-        }
-
-        calculateExpression("1 - 1").let {
-            assertThat(it).isEqualTo(0.0)
-        }
-
-        calculateExpression("3 * 3").let {
-            assertThat(it).isEqualTo(9.0)
-        }
-
-        calculateExpression("4 / 2").let {
-            assertThat(it).isEqualTo(2.0)
-        }
-
-        calculateExpression("4 / 2 * 10 + 10 * 10").let {
-            assertThat(it).isEqualTo(300.0)
-        }
+    @ParameterizedTest
+    @ValueSource(strings = ["", "  "])
+    fun `null 또는 공백 문자열에 대해 EmptyExpressionException 반환`(input: String) {
+        assertThatExceptionOfType(EmptyExpressionException::class.java)
+            .isThrownBy { calculate(input) }
+            .withMessage("수식은 null이거나 공백일 수 없습니다")
     }
 
     @Test
-    fun `에러 케이스 - null 입력`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression(null)
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
+    fun `잘못된 형식의 수식에 대해 InvalidExpressionException 반환`() {
+        assertThatExceptionOfType(InvalidExpressionException::class.java)
+            .isThrownBy { calculate("2 + + 3") }
+            .withMessage("잘못된 수식 형식입니다")
     }
 
     @Test
-    fun `에러 케이스 - 공백 입력`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression("   ")
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
+    fun `잘못된 숫자에 대해 InvalidNumberException 반환`() {
+        assertThatExceptionOfType(InvalidNumberException::class.java)
+            .isThrownBy { calculate("2 + a") }
+            .withMessage("잘못된 숫자: a")
     }
 
     @Test
-    fun `에러 케이스 - 부적절한 문자 포함`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression("2 + 3 @ 4")
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
+    fun `잘못된 연산자에 대해 InvalidOperatorException 반환`() {
+        assertThatExceptionOfType(InvalidOperatorException::class.java)
+            .isThrownBy { calculate("2 ^ 3") }
+            .withMessage("잘못된 연산자: ^")
     }
 
     @Test
-    fun `에러 케이스 - 정상적인 소수가 아닌 경우`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression("2.2.2 + 3")
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
+    fun `0으로 나눌 때 DivisionByZeroException 반환`() {
+        assertThatExceptionOfType(DivisionByZeroException::class.java)
+            .isThrownBy { calculate("10 / 0") }
+            .withMessage("0으로 나눌 수 없습니다")
     }
 
     @Test
-    fun `에러 케이스 - 사칙 연산 기호가 연속으로 나오는 경우`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression("2 ++ 3")
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
-    }
-
-    @Test
-    fun `에러 케이스 - 0으로 나누는 경우`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression("2 / 0")
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
-    }
-
-    @Test
-    fun `에러 케이스 - 잘못된 표현식`() {
-        val exception = assertThrows<IllegalArgumentException> {
-            calculateExpression("2 +")
-        }
-        val actualMessage = exception.message
-        println(actualMessage)
+    fun `정상적인 계산 상황`() {
+        assertThat(calculate("2 + 3 * 4 / 2")).isEqualTo(10.0)
+        assertThat(calculate("3 + 5 / 2")).isEqualTo(4.0)
+        assertThat(calculate("10 - 2 * 2")).isEqualTo(16.0)
+        assertThat(calculate("10 * 10 * 10")).isEqualTo(1000.0)
     }
 }
