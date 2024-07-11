6a45af98d96218de22f6ba07fa828c54ca2135ac
rtf400 <rtf400@naver.com>
2024-07-12 00:02:59 +0900

    <코드리뷰> (Step3) : 자동차 경주
    원시값 포장

--- src/main/kotlin/CarRacing/RacingCar.kt
+++ src/main/kotlin/CarRacing/RacingCar.kt
@@ -2,10 +2,15 @@ package CarRacing
 
 import kotlin.random.Random
 
-data class RacingCar(var progress: Int = 0) {
+const val INITIAL_PROGRESS = 0
+const val RANDOM_STT = 0
+const val RANDOM_END = 9
+const val THRESHOLD_VALUE = 4
+
+data class RacingCar(var progress: Int = INITIAL_PROGRESS) {
     fun run() {
-        val random = Random.nextInt(0, 9)
-        if (random >= 4)
+        val random = Random.nextInt(RANDOM_STT, RANDOM_END)
+        if (random >= THRESHOLD_VALUE)
             progress += 1
     }
 }
