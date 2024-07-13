fe2b30a7976d517e54b213bc4d71eb8e51388d85
rtf400 <rtf400@naver.com>
2024-07-14 02:14:02 +0900

    <추가> (Step4) : 자동차 경주
    IO 내용 수정

--- src/main/kotlin/CarRacing/InputView.kt
+++ src/main/kotlin/CarRacing/InputView.kt
@@ -1,27 +1,31 @@
 package CarRacing
 
-data class InputParameters(val numberOfCar: Int, val numberOfRace: Int)
+data class InputParameters(val carNames: List<String>, val numberOfRace: Int)
 
 object InputView {
-    private const val PROMPT_GET_NUMBER_OF_CAR = "자동차 대수는 몇 대인가요?"
+    private const val PROMPT_GET_CAR_NAMES = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분):"
     private const val PROMPT_GET_NUMBER_OF_RACE = "시도할 횟수는 몇 회인가요?"
 
+    private const val ERR_INPUT_CAR_NAMES = "자동차 이름 입력 실패"
+    private const val ERR_NAME_LENGTH = "자동차 이름은 5자를 초과할 수 없습니다"
     private const val ERR_INPUT_POSITIVE_INTEGER = "양의 정수 입력 실패"
-    const val ERR_INPUT_NUMBER_OF_CAR = "자동차 대수 입력이 정상적이지 않습니다."
-    const val ERR_INPUT_NUMBER_OF_RACE = "시도 횟수 입력이 정상적이지 않습니다."
+    private const val ERR_INPUT_NUMBER_OF_RACE = "시도 횟수 입력이 정상적이지 않습니다."
 
     fun run(): InputParameters {
-        val numberOfCar = getNumberOfCar()
+        val numberOfCar = getCarNames()
         val numberOfRace = getNumberOfRace()
         return InputParameters(numberOfCar, numberOfRace)
     }
-    private fun getNumberOfCar(): Int {
-        println(PROMPT_GET_NUMBER_OF_CAR)
-        try {
-            return getPositiveInteger()
-        } catch (e: IllegalArgumentException) {
-            throw IllegalArgumentException(ERR_INPUT_NUMBER_OF_CAR)
+    private fun getCarNames(): List<String> {
+        println(PROMPT_GET_CAR_NAMES)
+        val input = readlnOrNull()
+        require(input != null) { ERR_INPUT_CAR_NAMES }
+        val carNames = input.split(",").map { it.trim() }
+
+        carNames.forEach { name ->
+            require(name.length <= 5) { ERR_NAME_LENGTH }
         }
+        return carNames
     }
 
     private fun getNumberOfRace(): Int {
