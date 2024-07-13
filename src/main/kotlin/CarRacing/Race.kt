de7f7d7ba94adbadb0f1c36d39b3ec6a1762c52d
rtf400 <rtf400@naver.com>
2024-07-14 02:13:41 +0900

    <추가> (Step4) : 자동차 경주
    Race를 우승자 요구사항에 맞게 수정

--- src/main/kotlin/CarRacing/Race.kt
+++ src/main/kotlin/CarRacing/Race.kt
@@ -1,11 +1,18 @@
 package CarRacing
 
 enum class RaceErrorMessage(val message: String) {
-    OVER_TRY("시도 횟수 초과");
+    NO_CARS("차량 없음");
 }
 
 class Race(carNames: List<String> = List(3) { "pobi" }, val racingCars: List<RacingCar> = carNames.map { RacingCar(it) }) {
     fun tryRace() {
         racingCars.forEach { racingCar -> racingCar.run() }
+    }
+
+    fun getWinners(): List<String> {
+        val maxLength = racingCars.maxOfOrNull { it.progress }
+        require(maxLength != null) { RaceErrorMessage.NO_CARS }
+        val filteredCars = racingCars.filter { it.progress == maxLength }
+        return filteredCars.map { it.carName }
     }
 }
