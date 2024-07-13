03c7ad40f62c70fd2be68f3a5f7bf6a6142cfbeb
rtf400 <rtf400@naver.com>
2024-07-14 00:57:48 +0900

    <코드리뷰> (Step3) : 자동차 경주
    도메인 로직 관련 클래스는 데이터 클래스가 아닌 일반클래스로 (Race, RacingCar)

--- src/main/kotlin/CarRacing/Race.kt
+++ src/main/kotlin/CarRacing/Race.kt
@@ -4,7 +4,7 @@ enum class RaceErrorMessage(val message: String) {
     OVER_TRY("시도 횟수 초과");
 }
 
-data class Race(val numberOfCar: Int) {
+class Race(val numberOfCar: Int) {
     val racingCars: List<RacingCar> = List(numberOfCar) { RacingCar() }
 
     fun tryRace() {
