acbb65aa647f267068f7c6e19c7cfc3999b8f031
rtf400 <rtf400@naver.com>
2024-07-14 02:13:18 +0900

    <추가> (Step4) : 자동차 경주
    Race, RacingCar를 자동차 이름 요구사항에 맞게 수정

--- src/main/kotlin/CarRacing/Race.kt
+++ src/main/kotlin/CarRacing/Race.kt
@@ -4,8 +4,7 @@ enum class RaceErrorMessage(val message: String) {
     OVER_TRY("시도 횟수 초과");
 }
 
-class Race(val numberOfCar: Int, val racingCars: List<RacingCar> = List(numberOfCar) { RacingCar() }) {
-
+class Race(carNames: List<String> = List(3) { "pobi" }, val racingCars: List<RacingCar> = carNames.map { RacingCar(it) }) {
     fun tryRace() {
         racingCars.forEach { racingCar -> racingCar.run() }
     }
--- src/main/kotlin/CarRacing/RacingCar.kt
+++ src/main/kotlin/CarRacing/RacingCar.kt
@@ -1,9 +1,10 @@
 package CarRacing
 
+const val NA_NAME = "NO_NAME"
 const val INITIAL_PROGRESS = 0
 const val THRESHOLD_VALUE = 4
 
-class RacingCar(var progress: Int = INITIAL_PROGRESS, val carEngine: CarEngine = CarEngine()) {
+class RacingCar(val carName: String = NA_NAME, var progress: Int = INITIAL_PROGRESS, val carEngine: CarEngine = CarEngine()) {
     fun run() {
         if (carEngine.isRunnable())
             progress += 1
