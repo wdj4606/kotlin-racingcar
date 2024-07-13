633af82eb59a3d3a463250fffb9c6f1561eb6a29
rtf400 <rtf400@naver.com>
2024-07-14 02:14:30 +0900

    <추가> (Step4) : 자동차 경주
    관련 테스트 코드 수정

--- src/test/kotlin/RaceTest.kt
+++ src/test/kotlin/RaceTest.kt
@@ -10,14 +10,15 @@ import org.junit.jupiter.api.Test
 class RaceTest {
     @Test
     fun `우등 자동차 경주 잘 진행되니?`() {
-        val inputParameters = InputParameters(3, 5)
+        val carNames: List<String> = listOf("pobi", "crong", "honux")
+        val inputParameters = InputParameters(carNames, 5)
         val goodEngine = mockk<CarEngine>()
         every { goodEngine.isRunnable() } returns true
         val racingCar1 = RacingCar(carEngine = goodEngine)
         val racingCar2 = RacingCar(carEngine = goodEngine)
         val racingCar3 = RacingCar(carEngine = goodEngine)
         val racingCars: List<RacingCar> = listOf(racingCar1, racingCar2, racingCar3)
-        val race = Race(inputParameters.numberOfCar, racingCars)
+        val race = Race(racingCars = racingCars)
 
         repeat(inputParameters.numberOfRace) {
             race.tryRace()
@@ -30,14 +31,15 @@ class RaceTest {
 
     @Test
     fun `열등 자동차 경주 잘 진행되니?`() {
-        val inputParameters = InputParameters(3, 5)
+        val carNames: List<String> = listOf("pobi", "crong", "honux")
+        val inputParameters = InputParameters(carNames, 5)
         val badEngine = mockk<CarEngine>()
         every { badEngine.isRunnable() } returns false
         val racingCar1 = RacingCar(carEngine = badEngine)
         val racingCar2 = RacingCar(carEngine = badEngine)
         val racingCar3 = RacingCar(carEngine = badEngine)
         val racingCars: List<RacingCar> = listOf(racingCar1, racingCar2, racingCar3)
-        val race = Race(inputParameters.numberOfCar, racingCars)
+        val race = Race(inputParameters.carNames, racingCars)
 
         repeat(inputParameters.numberOfRace) {
             race.tryRace()
