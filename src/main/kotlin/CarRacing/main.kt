da5955cbf17e0b3372eb133bbfe6adba6a159d49
rtf400 <rtf400@naver.com>
2024-07-14 02:14:20 +0900

    <추가> (Step4) : 자동차 경주
    main 함수 수정

--- src/main/kotlin/CarRacing/main.kt
+++ src/main/kotlin/CarRacing/main.kt
@@ -4,11 +4,12 @@ import CarRacing.ResultView
 
 fun main() {
     val inputParameters = InputView.run()
-    val race = Race(inputParameters.numberOfCar)
+    val race = Race(inputParameters.carNames)
 
     ResultView.showStart()
     repeat(inputParameters.numberOfRace) {
         race.tryRace()
         ResultView.showRaceSnapShot(race)
     }
+    ResultView.showResult(race)
 }
