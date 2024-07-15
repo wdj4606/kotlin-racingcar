0008279c3d2a6df5792727408df18931b6c79680
pcs0807 <pcs0807@midasit.com>
2024-07-15 22:17:48 +0900

    <Step4> (피드백) : 불필요 테스트케이스 제거 및 기본 테스트 케이스 수정

--- src/test/kotlin/step4/RaceGameTest.kt
+++ src/test/kotlin/step4/RaceGameTest.kt
@@ -1,33 +1,8 @@
 package step4
 
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import step4.ExceptionType.ERROR_TRY_COUNT
-import java.io.ByteArrayInputStream
 
 class RaceGameTest {
-    @Test
-    fun `자동차 이름을 입력받고 쉼표로 구분된 리스트를 반환한다`() {
-        val input = "Peter, Jin, Makto"
-        val inputStream = ByteArrayInputStream(input.toByteArray())
-        System.setIn(inputStream)
-
-        val result = InputView.inputCarNames()
-
-        assertEquals(listOf("Peter", "Jin", "Makto"), result)
-    }
-
-    @Test
-    fun `실행 횟수가 유효하지 않을 경우 에러를 띄운다`() {
-        val input = "abc"
-        val inputStream = ByteArrayInputStream(input.toByteArray())
-        System.setIn(inputStream)
-
-        val exception = org.junit.jupiter.api.assertThrows<IllegalArgumentException> {
-            InputView.inputTryCount()
-        }
-        assertEquals(ERROR_TRY_COUNT, exception.message)
-    }
 
     @Test
     fun `게임 테스트`() {
@@ -36,6 +11,8 @@ class RaceGameTest {
         val cars = carNames.map { Car(it) }
         val game = RaceGame(cars, tryCount)
 
-        game.playGame()
+        val winners = game.playGame()
+        ResultView.printHistory(game.getRaceHistory())
+        ResultView.printWinners(winners)
     }
 }
