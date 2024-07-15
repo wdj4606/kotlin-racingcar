3592e8802111a581a17f5bcaf9857eeab479aff1
pcs0807 <pcs0807@midasit.com>
2024-07-16 00:45:22 +0900

    <Step5> 테스트 코드 작성

--- src/test/kotlin/step4/RaceGameTest.kt
+++ src/test/kotlin/step4/RaceGameTest.kt
@@ -1,6 +1,12 @@
 package step4
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import step4.domain.Car
+import step4.domain.RaceGame
+import step4.domain.RacingCar
+import step4.domain.Randomizer
+import step4.view.ResultView
 
 class RaceGameTest {
 
@@ -8,11 +14,33 @@ class RaceGameTest {
     fun `게임 테스트`() {
         val carNames = listOf("Peter", "Jin", "Makto")
         val tryCount = 5
-        val cars = carNames.map { Car(it) }
+        val cars = carNames.map { RacingCar(it) }
         val game = RaceGame(cars, tryCount)
 
         val winners = game.playGame()
         ResultView.printHistory(game.getRaceHistory())
         ResultView.printWinners(winners)
+    }
+
+    @Test
+    fun `car 생성 시 이름 유효성 검사`() {
+        val invalidName = "5글자가넘는이름"
+        val validName = "12345"
+
+        assertThrows<IllegalArgumentException> {
+            Car(invalidName)
+        }
+
+        Car(validName)
+    }
+
+    @Test
+    fun `getRandom 함수 테스트`() {
+        val randomNumber = Randomizer.getRandom()
+
+        assert(randomNumber in Randomizer.RANDOM_MIN..Randomizer.RANDOM_MAX) {
+            "생성된 숫자 $randomNumber 는 범위를 벗어났습니다. " +
+                "[${Randomizer.RANDOM_MIN}, ${Randomizer.RANDOM_MAX}]"
+        }
     }
 }
